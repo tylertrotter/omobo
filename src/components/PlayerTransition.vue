11043251f102247b03ef58e4f26789fe3ea3b48e
Tyler Trotter <tyler@cognitoforms.com>
2019-02-24 20:27:07 -0500

    Finish up transition screen.

--- src/components/PlayerTransition.vue
+++ src/components/PlayerTransition.vue
@@ -1,13 +1,23 @@
 <template>
-	<div class="player-transition" :class="{'active': $store.getters.turnTick === 0}" :style="'background: '+$store.getters.currentPlayer.color">
-		{{ $store.getters.currentPlayer.name }}
-		<button @click="remove">Ok</button>
+	<div @click="remove" class="player-transition" :class="{'active': $store.getters.turnTick === 0}">
+		<div class="mooko">{{ $store.getters.currentPlayer.name }}</div>
+		<avatar
+				:flesh="this.$store.getters.currentPlayer.color"
+				:iris="this.$store.getters.currentPlayer.avatar.iris"
+				:headHeight="this.$store.getters.currentPlayer.avatar.headHeight"
+				:headWidth="this.$store.getters.currentPlayer.avatar.headWidth"
+				:eyeSize="this.$store.getters.currentPlayer.avatar.eyeSize"
+				:class="'emotion--' + $store.getters.currentPlayer.avatar.emotion"
+				class="transition-screen-avatar"
+			/>
 	</div>
 </template>
 
 <script>
+	import avatar from './Avatar.vue';
 	export default {
 		name: 's-player-transition',
+		components: { avatar },
 		methods: {
 			remove(){
 				this.$el.classList.remove('active');
@@ -23,10 +33,29 @@
 		bottom: 0;
 		left: 0;
 		right: 100%;
+		background: rgba(0,0,0,.8);
 		overflow: hidden;
 		transition: all .6s;
 	}
 	.player-transition.active{
 		right: 0;
+	}
+
+	.player-transition .mooko {
+		position: absolute;
+		right: 200%;
+		color: white;
+		font-size: 13vw;
+		transition: right 1s;
+	}
+
+	.player-transition.active .mooko{
+		right: 0;
+	}
+
+	.transition-screen-avatar {
+		width: 100vh;
+    display: block;
+		transition: all 1s;
 	}
 </style>
